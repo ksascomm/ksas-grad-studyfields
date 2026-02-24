7ca9a4c63e81c9c9864b9f88a34ace6d9dec356d
ksascomm <ksasweb@jhu.edu>
2026-02-24 10:04:19 -0500

    Version 2.0: Code update

--- ksas-graduate-fields.php
+++ ksas-graduate-fields.php
@@ -1,259 +1,189 @@
 <?php
-/*
-Plugin Name: KSAS Graduate Fields
-Plugin URI: https://krieger.jhu.edu
-Description: Graduate Fields of Study
-Version: 1.0
-Author: KSAS Communications
-Author URI: mailto:ksasweb@jhu.edu
-License: GPL2
-*/
+/**
+ * KSAS Graduate Fields
+ *
+ * @package     KSAS_Graduate_Fields
+ * @author      KSAS Communications
+ * @license     GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name: KSAS Graduate Fields
+ * Description: Graduate Fields of Study with full security and PHPCS compliance.
+ * Version:     2.0
+ * Author:      KSAS Communications
+ * Text Domain:  ksas-grad-fields
+ */
 
-// registration code for Graduate Fields of Study post type
-	function register_gradstudyfields_posttype() {
-		$labels = array(
-			'name' 				=> _x( 'Graduate Fields of Study', 'post type general name' ),
-			'singular_name'		=> _x( 'Graduate Field of Study', 'post type singular name' ),
-			'add_new' 			=> __( 'Add New Graduate Field of Study' ),
-			'add_new_item' 		=> __( 'Add New Graduate Field of Study' ),
-			'edit_item' 		=> __( 'Edit Graduate Field of Study' ),
-			'new_item' 			=> __( 'New Graduate Field of Study' ),
-			'view_item' 		=> __( 'View Graduate Field of Study' ),
-			'search_items' 		=> __( 'Search Graduate Fields of Study' ),
-			'not_found' 		=> __( 'No Graduate Fields of Study found' ),
-			'not_found_in_trash'=> __( 'No Graduate Fields of Study found in Trash' ),
-			'parent_item_colon' => __( '' ),
-			'menu_name'			=> __( 'Graduate Fields of Study' )
-		);
-				
-		$supports = array('title');
-		
-		$post_type_args = array(
-			'labels' 			=> $labels,
-			'singular_label' 	=> __('Graduate Field of Study'),
-			'public' 			=> true,
-			'show_ui' 			=> true,
-			'publicly_queryable'=> true,
-			'query_var'			=> true,
-			'capability_type' 	=> 'post',
-			'has_archive' 		=> false,
-			'rewrite' 			=> array('slug' => 'programs', 'with_front' => false ),
-			'supports' 			=> $supports,
-			'menu_position' 	=> 5,
-			'show_in_nav_menus' => true,
-		 );
-		 register_post_type('gradstudyfields',$post_type_args);
-	}
-	add_action('init', 'register_gradstudyfields_posttype');
-
-$basicinformation_5_metabox = array( 
-	'id' => 'basicinformation',
-	'title' => 'Basic Information',
-	'page' => array('gradstudyfields'),
-	'context' => 'normal',
-	'priority' => 'default',
-	'fields' => array(
-
-				array(
-					'name' 			=> 'Department Website',
-					'desc' 			=> 'We recommend linking to the Graduate section',
-					'id' 				=> 'ecpt_website',
-					'class' 			=> 'ecpt_website',
-					'type' 			=> 'text',
-					'rich_editor' 	=> 0,			
-					'max' 			=> 0,
-					'std'			=> ''				
-				),
-
-				array(
-					'name' 			=> 'Contact Name',
-					'desc' 			=> '',
-					'id' 				=> 'ecpt_contactname',
-					'class' 			=> 'ecpt_contactname',
-					'type' 			=> 'text',
-					'rich_editor' 	=> 0,			
-					'max' 			=> 0,
-					'std'			=> ''				
-				),
-				
-				array(
-					'name' 			=> 'Phone Number',
-					'desc' 			=> '',
-					'id' 			=> 'ecpt_phonenumber',
-					'class' 		=> 'ecpt_phonenumber',
-					'type' 			=> 'text',
-					'rich_editor' 	=> 0,			
-					'max' 			=> 0,
-					'default'		=> '',
-					'std'			=> ''					
-				),
-							
-				array(
-					'name' 			=> 'Email Address',
-					'desc' 			=> '',
-					'id' 				=> 'ecpt_emailaddress',
-					'class' 			=> 'ecpt_emailaddress',
-					'type' 			=> 'text',
-					'rich_editor' 	=> 0,			
-					'max' 			=> 0,
-					'default'		=> '',
-					'std'			=> ''						
-				),
-
-				array(
-					'name' 			=> 'Deadline',
-					'desc' 			=> 'Please type month in abbreviated format (Jan. 15)',
-					'id' 				=> 'ecpt_deadline',
-					'class' 			=> 'ecpt_deadline',
-					'type' 			=> 'text',
-					'rich_editor' 	=> 0,			
-					'max' 			=> 0,
-					'std'			=> ''						
-				),	
-
-				array(
-					'name' 			=> 'Additional Deadlines',
-					'desc' 			=> 'Please enter degree type, and month in abbreviated format (MA Spring Admission: Jan. 15)',
-					'id' 				=> 'ecpt_adddeadline',
-					'class' 			=> 'ecpt_adddeadline',
-					'type' 			=> 'text',
-					'rich_editor' 	=> 0,			
-					'max' 			=> 0,
-					'std'			=> ''			
-				),			
-				
-				array(
-					'name' 			=> 'Degrees Offered',
-					'desc' 			=> 'Separate with commas',
-					'id' 				=> 'ecpt_degreesoffered',
-					'class' 			=> 'ecpt_degreesoffered',
-					'type' 			=> 'text',
-					'rich_editor' 	=> 1,			
-					'max' 			=> 0,
-					'default'		=> '',
-					'std'			=> ''		
-				),
-				array(
-					'name' 			=> 'Supplemental Materials',
-					'desc' 			=> '',
-					'id' 				=> 'ecpt_supplementalmaterials',
-					'class' 			=> 'ecpt_supplementalmaterials',
-					'type' 			=> 'textarea',
-					'rich_editor' 	=> 0,			
-					'max' 			=> 0,
-					'default'			=> '',
-					'std'			=> ''					
-				),
-		)								
-);			
-			
-add_action('admin_menu', 'ecpt_add_basicinformation_5_meta_box');
-function ecpt_add_basicinformation_5_meta_box() {
-
-	global $basicinformation_5_metabox;		
-
-	foreach($basicinformation_5_metabox['page'] as $page) {
-		add_meta_box($basicinformation_5_metabox['id'], $basicinformation_5_metabox['title'], 'ecpt_show_basicinformation_5_box', $page, 'normal', 'default', $basicinformation_5_metabox);
-	}
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-// function to show meta boxes
-function ecpt_show_basicinformation_5_box()	{
-	global $post;
-	global $basicinformation_5_metabox;
-	global $ecpt_prefix;
-	global $wp_version;
-	
-	// Use nonce for verification
-	echo '<input type="hidden" name="ecpt_basicinformation_5_meta_box_nonce" value="', wp_create_nonce(basename(__FILE__)), '" />';
-	
-	echo '<table class="form-table">';
+/**
+ * 1. POST TYPE REGISTRATION
+ */
 
-	foreach ($basicinformation_5_metabox['fields'] as $field) {
-		// get current post meta data
+/**
+ * Registers the 'gradstudyfields' post type.
+ *
+ * @return void
+ */
+function ksas_register_gradstudyfields_posttype() {
+	$labels = array(
+		'name'          => _x( 'Graduate Fields of Study', 'post type general name', 'ksas-grad-fields' ),
+		'singular_name' => _x( 'Graduate Field of Study', 'post type singular name', 'ksas-grad-fields' ),
+		'add_new'       => __( 'Add New Graduate Field of Study', 'ksas-grad-fields' ),
+		'add_new_item'  => __( 'Add New Graduate Field of Study', 'ksas-grad-fields' ),
+		'edit_item'     => __( 'Edit Graduate Field of Study', 'ksas-grad-fields' ),
+		'menu_name'     => __( 'Graduate Fields', 'ksas-grad-fields' ),
+	);
 
-		$meta = get_post_meta($post->ID, $field['id'], true);
-		
-		echo '<tr>',
-				'<th style="width:20%"><label for="', $field['id'], '">', stripslashes($field['name']), '</label></th>',
-				'<td class="ecpt_field_type_' . str_replace(' ', '_', $field['type']) . '">';
-		switch ($field['type']) {
-			case 'text':
-				echo '<input type="text" name="', $field['id'], '" id="', $field['id'], '" value="', $meta ? $meta : $field['std'], '" size="30" style="width:97%" /><br/>', '', stripslashes($field['desc']);
-				break;
-			case 'textarea':
-			
-				if($field['rich_editor'] == 1) {
-						echo wp_editor($meta, $field['id'], array('textarea_name' => $field['id'], 'wpautop' => false)); }
-					 else {
-					echo '<div style="width: 100%;"><textarea name="', $field['id'], '" class="', $field['class'], '" id="', $field['id'], '" cols="60" rows="8" style="width:97%">', $meta ? $meta : $field['default'], '</textarea></div>', '', stripslashes($field['desc']);				
-				}
-				
-				break;
-			case 'select':
-				echo '<select name="', $field['id'], '" id="', $field['id'], '">';
-				foreach ($field['options'] as $option) {
-					echo '<option value="' . $option . '"', $meta == $option ? ' selected="selected"' : '', '>', $option, '</option>';
-				}
-				echo '</select>', '', $field['desc'];
-				break;
-			case 'checkbox':
-				echo '<input type="checkbox" name="', $field['id'], '" id="', $field['id'], '"', $meta ? ' checked="checked"' : '', ' />&nbsp;';
-				echo $field['desc'];
-				break;							
-		}
-		echo     '<td>',
-			'</tr>';
+	register_post_type(
+		'gradstudyfields',
+		array(
+			'labels'        => $labels,
+			'public'        => true,
+			'show_ui'       => true,
+			'show_in_rest'  => true,
+			'supports'      => array( 'title' ),
+			'has_archive'   => false,
+			'rewrite'       => array(
+				'slug'       => 'programs',
+				'with_front' => false,
+			),
+			'menu_position' => 5,
+		)
+	);
+}
+add_action( 'init', 'ksas_register_gradstudyfields_posttype' );
+
+/**
+ * 2. META BOX REGISTRATION
+ */
+
+/**
+ * Adds the 'Basic Information' meta box.
+ *
+ * @return void
+ */
+function ksas_add_grad_meta_boxes() {
+	add_meta_box(
+		'ksas_basic_info',
+		__( 'Basic Information', 'ksas-grad-fields' ),
+		'ksas_render_grad_meta_box',
+		'gradstudyfields',
+		'normal',
+		'default'
+	);
+}
+add_action( 'add_meta_boxes', 'ksas_add_grad_meta_boxes' );
+
+/**
+ * Renders the meta box HTML.
+ *
+ * @param WP_Post $post The current post object.
+ * @return void
+ */
+function ksas_render_grad_meta_box( $post ) {
+	wp_nonce_field( 'ksas_grad_meta_nonce_action', 'ksas_grad_meta_nonce_field' );
+
+	$fields = array(
+		'ecpt_website'               => get_post_meta( $post->ID, 'ecpt_website', true ),
+		'ecpt_contactname'           => get_post_meta( $post->ID, 'ecpt_contactname', true ),
+		'ecpt_phonenumber'           => get_post_meta( $post->ID, 'ecpt_phonenumber', true ),
+		'ecpt_emailaddress'          => get_post_meta( $post->ID, 'ecpt_emailaddress', true ),
+		'ecpt_deadline'              => get_post_meta( $post->ID, 'ecpt_deadline', true ),
+		'ecpt_adddeadline'           => get_post_meta( $post->ID, 'ecpt_adddeadline', true ),
+		'ecpt_degreesoffered'        => get_post_meta( $post->ID, 'ecpt_degreesoffered', true ),
+		'ecpt_supplementalmaterials' => get_post_meta( $post->ID, 'ecpt_supplementalmaterials', true ),
+	);
+	?>
+	<table class="form-table">
+		<tr>
+			<th><label for="ecpt_website"><?php esc_html_e( 'Department Website', 'ksas-grad-fields' ); ?></label></th>
+			<td>
+				<input type="url" name="ecpt_website" id="ecpt_website" value="<?php echo esc_url( $fields['ecpt_website'] ); ?>" class="widefat" />
+				<p class="description"><?php esc_html_e( 'Please link to the Graduate section of your department website.', 'ksas-grad-fields' ); ?></p>
+			</td>
+		</tr>
+		<tr>
+			<th><label for="ecpt_contactname"><?php esc_html_e( 'Contact Name', 'ksas-grad-fields' ); ?></label></th>
+			<td><input type="text" name="ecpt_contactname" id="ecpt_contactname" value="<?php echo esc_attr( $fields['ecpt_contactname'] ); ?>" class="widefat" /></td>
+		</tr>
+		<tr>
+			<th><label for="ecpt_phonenumber"><?php esc_html_e( 'Phone Number', 'ksas-grad-fields' ); ?></label></th>
+			<td><input type="text" name="ecpt_phonenumber" id="ecpt_phonenumber" value="<?php echo esc_attr( $fields['ecpt_phonenumber'] ); ?>" class="widefat" /></td>
+		</tr>
+		<tr>
+			<th><label for="ecpt_emailaddress"><?php esc_html_e( 'Email Address', 'ksas-grad-fields' ); ?></label></th>
+			<td><input type="email" name="ecpt_emailaddress" id="ecpt_emailaddress" value="<?php echo esc_attr( $fields['ecpt_emailaddress'] ); ?>" class="widefat" /></td>
+		</tr>
+		<tr>
+			<th><label for="ecpt_deadline"><?php esc_html_e( 'Deadline', 'ksas-grad-fields' ); ?></label></th>
+			<td>
+				<input type="text" name="ecpt_deadline" id="ecpt_deadline" value="<?php echo esc_attr( $fields['ecpt_deadline'] ); ?>" class="widefat" />
+				<p class="description"><?php esc_html_e( 'Please type month in abbreviated format (e.g. Jan. 15)', 'ksas-grad-fields' ); ?></p>
+			</td>
+		</tr>
+		<tr>
+			<th><label for="ecpt_adddeadline"><?php esc_html_e( 'Additional Deadlines', 'ksas-grad-fields' ); ?></label></th>
+			<td>
+				<input type="text" name="ecpt_adddeadline" id="ecpt_adddeadline" value="<?php echo esc_attr( $fields['ecpt_adddeadline'] ); ?>" class="widefat" />
+				<p class="description"><?php esc_html_e( 'Please enter degree type, and month in abbreviated format (MA Spring Admission: Jan. 15)', 'ksas-grad-fields' ); ?></p>
+			</td>
+		</tr>
+		<tr>
+			<th><label for="ecpt_degreesoffered"><?php esc_html_e( 'Degrees Offered', 'ksas-grad-fields' ); ?></label></th>
+			<td>
+				<input type="text" name="ecpt_degreesoffered" id="ecpt_degreesoffered" value="<?php echo esc_attr( $fields['ecpt_degreesoffered'] ); ?>" class="widefat" />
+				<p class="description"><?php esc_html_e( 'Separate with commas', 'ksas-grad-fields' ); ?></p>
+			</td>
+		</tr>
+		<tr>
+			<th><label for="ecpt_supplementalmaterials"><?php esc_html_e( 'Supplemental Materials', 'ksas-grad-fields' ); ?></label></th>
+			<td><textarea name="ecpt_supplementalmaterials" id="ecpt_supplementalmaterials" rows="5" class="widefat"><?php echo esc_textarea( $fields['ecpt_supplementalmaterials'] ); ?></textarea></td>
+		</tr>
+	</table>
+	<?php
+}
+
+/**
+ * 4. SECURE SAVE LOGIC
+ *
+ * @param int $post_id The ID of the post being saved.
+ * @return void
+ */
+function ksas_save_grad_meta_box_data( $post_id ) {
+	// Verify nonce.
+	$nonce_field = isset( $_POST['ksas_grad_meta_nonce_field'] ) ? sanitize_text_field( wp_unslash( $_POST['ksas_grad_meta_nonce_field'] ) ) : '';
+
+	if ( ! wp_verify_nonce( $nonce_field, 'ksas_grad_meta_nonce_action' ) ) {
+		return;
 	}
-	
-	echo '</table>';
-}	
 
-// Save data from meta box
-add_action('save_post', 'ecpt_basicinformation_5_save');
-function ecpt_basicinformation_5_save($post_id) {
-	global $post;
-	global $basicinformation_5_metabox;
-	
-	// verify nonce
-	if (!isset($_POST['ecpt_basicinformation_5_meta_box_nonce']) || !wp_verify_nonce($_POST['ecpt_basicinformation_5_meta_box_nonce'], basename(__FILE__))) {
-		return $post_id;
+	if ( ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) || ! current_user_can( 'edit_post', $post_id ) ) {
+		return;
 	}
 
-	// check autosave
-	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-		return $post_id;
-	}
+	$field_keys = array(
+		'ecpt_website'               => 'esc_url_raw',
+		'ecpt_contactname'           => 'sanitize_text_field',
+		'ecpt_phonenumber'           => 'sanitize_text_field',
+		'ecpt_emailaddress'          => 'sanitize_email',
+		'ecpt_deadline'              => 'sanitize_text_field',
+		'ecpt_adddeadline'           => 'sanitize_text_field',
+		'ecpt_degreesoffered'        => 'sanitize_text_field',
+		'ecpt_supplementalmaterials' => 'sanitize_textarea_field',
+	);
 
-	// check permissions
-	if ('page' == $_POST['post_type']) {
-		if (!current_user_can('edit_page', $post_id)) {
-			return $post_id;
-		}
-	} elseif (!current_user_can('edit_post', $post_id)) {
-		return $post_id;
-	}
-	
-	foreach ($basicinformation_5_metabox['fields'] as $field) {
-	
-		$old = get_post_meta($post_id, $field['id'], true);
-		$new = $_POST[$field['id']];
-		
-		if ($new && $new != $old) {
-			if($field['type'] == 'date') {
-				$new = ecpt_format_date($new);
-				update_post_meta($post_id, $field['id'], $new);
-			} else {
-				if(is_string($new)) {
-					$new = $new;
-				} 
-				update_post_meta($post_id, $field['id'], $new);
-				
-				
-			}
-		} elseif ('' == $new && $old) {
-			delete_post_meta($post_id, $field['id'], $old);
+	foreach ( $field_keys as $key => $sanitize_func ) {
+		if ( isset( $_POST[ $key ] ) ) {
+			// Wrapping unslash directly inside the call clears the "non-sanitized" input error in loops.
+			update_post_meta(
+				$post_id,
+				$key,
+				call_user_func( $sanitize_func, wp_unslash( $_POST[ $key ] ) )
+			);
 		}
 	}
 }
-?>
+add_action( 'save_post', 'ksas_save_grad_meta_box_data' );
